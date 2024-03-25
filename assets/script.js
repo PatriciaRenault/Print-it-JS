1afab7bff02c075f3b47e318e72c7ac34a1ebcd0
Patricia Renault <patricia.renault@ymail.com>
2024-03-25 18:36:13 +0100

    Ajout de commentaires

--- assets/script.js
+++ assets/script.js
@@ -18,56 +18,56 @@ const slides = [
 ]
 
 //déclaration des constantes et variables
-const img = document.querySelector(".banner-img")
-const texte = document.querySelector("#banner p")
-const nbreSlides = slides.length
+const img = document.querySelector(".banner-img")                    // sélectionne un élément avec la classe CSS "".banner-img".
+const texte = document.querySelector("#banner p")                    // sélectionne les paragraphs enfant de l'élément avec l'ID "banner". 
+const nbreSlides = slides.length                                     // stocke la longueur du tableau slides dans la variable nbreSlides
 
-let counter = 0
+let counter = 0                                                      // initialisation du compteur
 
 //Ajout des bullet points au slider
-//recuperation de l'element de classe "dots"
-const divDot = document.querySelector(".dots")
+
+const divDot = document.querySelector(".dots")                       //recuperation de l'element de classe "dots"
 
 for(let i = 0; i < nbreSlides; i++) {
-	//creation de la div
-	const Dot = document.createElement('div')
-	//ajout de la classe .dot
-	Dot.classList.add("dot")
-	//ajout de la div de classe .dot
-	divDot.appendChild(Dot)
+
+	const Dot = document.createElement('div')                        //creation de la div        
+
+	Dot.classList.add("dot")                                         //ajout de la classe .dot
+
+	divDot.appendChild(Dot)                                          //ajout de la div de classe .dot   => ajout du point dot                          
 }
-let Dots = document.querySelectorAll(".dot")
-Dots[0].classList.add("dot_selected")
+let Dots = document.querySelectorAll(".dot")                         //sélectionne tous les éléments avec la classe CSS .dot, 
+Dots[0].classList.add("dot_selected")                                //ajoute la classe .dot_selected au premier point pour indiquer que c'est la premiere diapo qui est affichée
 
 
 
-// eventlistener fleche suivante
-const next = document.querySelector(".arrow_right")
-next.addEventListener("click", () => {
-	counter++
-	if(counter === nbreSlides){
-		counter = 0
-		Dots[nbreSlides-1].classList.remove("dot_selected")
+// Ecouteur d'événements pour détecter un clic sur un élément avec la classe .arrow_right
+const next = document.querySelector(".arrow_right") 					//selectionne les éléments avec la classe "".arrow_right"
+next.addEventListener("click", () => {              					// Lorsque l'utilisateur clic sur l'element fleche droite, la fonction fléchée est exécutée
+	counter++                                       					// incrementation du compteur
+	if(counter === nbreSlides){                     					// si le compteur est egale au nombre de slide
+		counter = 0                                 					// on remet le compteur a 0
+		Dots[nbreSlides-1].classList.remove("dot_selected")             // on retire la class "dot_selected"
 	}
-	img.src = "./assets/images/slideshow/" + slides[counter].image
-	texte.innerHTML = slides[counter].tagLine
-	Dots[counter].classList.add("dot_selected")
-	Dots[counter-1].classList.remove("dot_selected")
+	img.src = "./assets/images/slideshow/" + slides[counter].image      // Met à jour l'attribut src de l'image avec le chemin de la nouvelle diapositive
+	texte.innerHTML = slides[counter].tagLine                           // Met à jour le texte avec la légende de la nouvelle diapositive
+	Dots[counter].classList.add("dot_selected")                         // Ajoute la classe "dot_selected" à la diapositive actuelle
+	Dots[counter-1].classList.remove("dot_selected")                    // Retire la classe "dot_selected" de la diapositive précédente
 
 })
 
 // eventlistener fleche precedente
-const back = document.querySelector(".arrow_left")
-back.addEventListener("click", () => {
-	counter--
-	if(counter < 0){ 
-		Dots[counter+1].classList.remove("dot_selected")
-		counter = nbreSlides - 1
+const back = document.querySelector(".arrow_left")                      // selectionne les éléments avec la classe "".arrow_right"
+back.addEventListener("click", () => {                                  // Lorsque l'utilisateur clic sur l'element fleche gauche, la fonction fléchée est exécutée
+	counter--                                                           // decrementation du compteur
+	if(counter < 0){                                                    // Si le compteur devient inférieur à zéro, cela signifie que l'utilisateur est revenu à la première diapositive.
+		Dots[counter+1].classList.remove("dot_selected")                // la classe dot_selected est retirée de la diapositive actuelle
+		counter = nbreSlides - 1                                        // le compteur est réinitialisé à la dernière diapositive
 	}
-	img.src = "./assets/images/slideshow/" + slides[counter].image
-	texte.innerHTML = slides[counter].tagLine
-	Dots[counter].classList.add("dot_selected")	
-	Dots[counter+1].classList.remove("dot_selected")								
+	img.src = "./assets/images/slideshow/" + slides[counter].image      // Met à jour l'attribut src de l'image avec le chemin de la nouvelle diapositive
+	texte.innerHTML = slides[counter].tagLine                           // Met à jour le texte avec la légende de la nouvelle diapositive
+	Dots[counter].classList.add("dot_selected")	                        // Ajoute la classe "dot_selected" à la diapositive actuelle
+	Dots[counter+1].classList.remove("dot_selected")	                // Retire la classe "dot_selected" de la diapositive précédente							
 })
 
 
